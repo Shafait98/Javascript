3512a2679c3c42567d0adb8e18584cf4d98fa0e5
Shafait98 <farhan98aiub@gmail.com>
2025-07-28 01:35:39 +0600

    exploring the data types of js

---
+++ type.js
@@ -0,0 +1,11 @@
+var name = "Farhan";
+var age = 26;
+var married = true;
+var phone = +881626866291;
+
+console.log(name);
+console.log(age);
+console.log(married);
+console.log(typeof married);
+console.log(phone); // for print the value
+console.log(typeof phone); // for know the data type
