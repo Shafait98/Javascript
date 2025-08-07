8ad7b3fa95aad747cb90139844ae6cd2ab1456f8
Shafait98 <farhan98aiub@gmail.com>
2025-08-07 17:55:25 +0600

    practicing js

---
+++ numbers.js
@@ -0,0 +1,23 @@
+// console.log("Grocery list");
+
+// var rice = 220;
+// // var meat = parseInt(1254.56);
+// var potato = 450;
+// // var greenChili =parseInt(28.55) ;
+// var blackPaper = 250;
+// // var onion = parseInt(65.66);
+
+// // console.log("total price: " + rice + meat + potato + greenChili + blackPaper + onion);
+// console.log(rice + potato +  blackPaper);
+
+
+var rice = 220;
+ var potato = 450;
+ var blackPaper = 250;
+
+var orange = 20;
+var apple = 40;
+
+console.log(rice + potato +  blackPaper);
+
+console.log(orange + apple);
---
+++ testNum.js
@@ -0,0 +1,12 @@
+var potato = 0.2;
+var onion = 0.1;
+
+var total = potato + onion;
+
+console.log(total.toFixed(4)); // defined how many decimal points it'll take
+
+
+var first = 20;
+var second = parseInt('20'); // convert string to integer
+
+console.log(first + second);
