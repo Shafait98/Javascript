d8f3e99aabe4f51623b5a1850cdef620b8de84c8
Shafait98 <farhan98aiub@gmail.com>
2025-08-07 20:35:54 +0600

    exploring the conditions of js

---
+++ comparison.js
@@ -0,0 +1,9 @@
+var grapePrice = 800;
+var applePrice = 350;
+var orangePrice = 600;
+
+// console.log(grapePrice > orangePrice);
+// console.log(orangePrice < applePrice);
+
+console.log(grapePrice != orangePrice);
+
---
+++ condition.js
@@ -0,0 +1,27 @@
+// var money = 300;
+
+// if(money > 500)
+// {
+//     console.log("we will go to cineplex");
+// }
+// else
+// {
+//     console.log("buy netflix subscription");
+
+// }
+
+let age = 18;
+
+if(age >= 18)
+{
+    console.log("You're permissible to let in")
+}
+else if(age == 18)
+{
+    console.log("You're welcome");
+}
+
+else 
+    {
+        console.log("Sorry! you're not even 18");
+    }
