f1a2cf89f9f64fd7b16e515a2b360fa2e7f4825e
Shafait98 <farhan98aiub@gmail.com>
2025-08-07 20:54:52 +0600

    exploring multiple conditions in if else statement

---
+++ multipleCondition.js
@@ -0,0 +1,14 @@
+const salary = 25000;
+const hasCar = true;
+const isBcs = false;
+const height = 5.8;
+
+if((salary >=25000 || isBcs == true) && hasCar == true && height >= 5)
+{
+    console.log("You're welcome");
+}
+
+else
+    {
+        console.log("muri khaw");
+    }
