7a50ffb01e4b384116fe0ee545b48ce41e9463a8
Shafait98 <farhan98aiub@gmail.com>
2025-08-09 21:14:16 +0600

    explore nested if else

---
+++ nestedCondition.js
@@ -0,0 +1,33 @@
+const money = 6;
+
+if (money > 300)
+{
+    console.log("Ma Sha Allah! you're in blessings");
+}
+
+else
+{
+    if(money > 100)
+    {
+        console.log("Ma Sha Allah, all we have");
+    }
+else 
+    {
+        if(money > 50)
+        {
+            console.log("You can buy buns & banana");
+        }
+    }
+    if(money > 20)
+    {
+        console.log("You can buy some chocolates!");
+    }
+
+    else
+    {
+        if(money > 0)
+        {
+            console.log("Save your money");
+        }
+    }
+}
