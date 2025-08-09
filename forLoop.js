a1e3ee765dc84503f13e61f556787df115ba86b5
Shafait98 <farhan98aiub@gmail.com>
2025-08-10 01:46:52 +0600

    exploring the for loop

---
+++ forLoop.js
@@ -0,0 +1,8 @@
+const fruits = ["Mango","Banana","jackfruit","Grape","Orange","Pineapple","Strawberry"];
+
+for(const fruit of fruits)
+{
+    console.log("i love: ");
+    console.log(fruit);
+    
+}
