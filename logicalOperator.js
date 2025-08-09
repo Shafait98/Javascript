7a8457b50491e111a5e44a24025ace7c9fc88d7b
Shafait98 <farhan98aiub@gmail.com>
2025-08-09 22:20:17 +0600

    exploring logical operator in conditions parameter

---
+++ logicalOperator.js
@@ -0,0 +1,21 @@
+flag = true;
+
+if(flag)     // for true value
+{
+    console.log("You're green flag");
+}
+else
+    {
+        console.log("you're red flag");
+    }
+
+
+    if(!flag)   // for false value
+    {
+        console.log("You're green flag");
+    }
+
+    else
+    {
+     console.log("You're red flag");
+    }
