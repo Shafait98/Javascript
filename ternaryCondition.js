8a67063c9c189b367e8be4b6abf3c73735120791
Shafait98 <farhan98aiub@gmail.com>
2025-08-09 21:59:22 +0600

    explore multilevel ternary conditions

--- ternaryCondition.js
+++ ternaryCondition.js
@@ -10,6 +10,33 @@ let price = 300;
 //     price = price + 100;
 // }
 
-price = isLeader===true ? 0 : price + 100;   // ternary condition
+
+// price = isLeader===true ? 0 : price + 100;   // ternary condition
+
+
+
+
+
+// if(isLeader===true)
+// {
+//     if(price>1000)
+//     {
+//         price = price / 2;
+//     }
+
+//     else
+//     {
+//     price = 0;
+//     }
+// }
+
+// else 
+// {
+//     price = price +100;
+// }
+
+
+price = isLeader===true ? price>1000 ? price/2: 0 : price + 100;
+
 
 console.log(price);
