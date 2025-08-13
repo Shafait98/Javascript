51c1270a617db614c45aa966d2d34ebfdeb44603
Shafait98 <farhan98aiub@gmail.com>
2025-08-13 15:20:02 +0600

    explored for loop & did sum op in loop

---
+++ forLoop.js
@@ -0,0 +1,19 @@
+// let i;
+
+
+// for(i = 0; i<=20; i++)
+// {
+//     console.log(i);
+// }
+
+
+let i;
+let sum = 0;
+
+for (i = 0; i <=20; i++)
+{
+    sum = sum + i;
+    console.log(i);
+}
+
+console.log("sum of all numbers: "+ sum);
