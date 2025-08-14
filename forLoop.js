bb3b59a73367ea81c7ed339e3d2b91c4acd73d89
Shafait98 <farhan98aiub@gmail.com>
2025-08-15 04:15:48 +0600

    explore even operation in for loop

--- forLoop.js
+++ forLoop.js
@@ -7,13 +7,30 @@
 // }
 
 
-let i;
-let sum = 0;
+//*********************..... */ sum operation in loop ****************/
 
-for (i = 0; i <=20; i++)
+
+// let i;
+// let sum = 0;
+
+// for (i = 0; i <=20; i++)
+// {
+//     sum = sum + i;
+//     console.log(i);
+// }
+
+// console.log("sum of all numbers: "+ sum);
+
+
+
+
+//*********************Even number */
+
+
+ console.log("Even numbers are: ");
+let i;
+for(i = 0; i<=20; i= i+2)
 {
-    sum = sum + i;
+   
     console.log(i);
 }
-
-console.log("sum of all numbers: "+ sum);
---
+++ function.js
@@ -0,0 +1,3 @@
+const greet = (name) => `Hello, ${name = 21 % 2 }!`;
+
+console.log(greet(1)); // Output: Hello, Bootcamp Learner!
