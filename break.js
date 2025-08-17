66dbe3621b2fb591e0007637b6f26ca92a067fca
Shafait98 <farhan98aiub@gmail.com>
2025-08-18 05:29:06 +0600

    explore break in loop & reverse looping

---
+++ break.js
@@ -0,0 +1,26 @@
+// break means the done with the loop. end of execution
+
+
+// for(let i = 0; i<=50; i++)
+// {
+    
+//     if(i >= 8)
+//     {
+//         break;
+//     }
+//     console.log(i);
+// }
+
+//**reverse loop & break */
+let n =800000;
+while(n > 50)
+{
+    if(n <= 60000)
+    {
+        break;
+    }
+    console.log(n);
+    n--;
+
+}
+
