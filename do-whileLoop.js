2873ff65ca9deeab471c30c063b104041ce2c8e3
Shafait98 <farhan98aiub@gmail.com>
2025-08-18 16:35:45 +0600

    explore do-while loop

---
+++ do-whileLoop.js
@@ -0,0 +1,26 @@
+// let i = 0;
+
+// do
+// {
+//   console.log(i);
+//   i++;
+// }
+
+// while(i <= 10)
+
+
+    //***Sum operation who do-while loop */
+
+    let i = 0;
+    let sum = 0;
+
+    do
+    {
+        console.log(i);
+        sum = sum + i;
+        i++;
+    }
+
+    while(i <= 30)
+    
+     console.log("Total = ",sum);
