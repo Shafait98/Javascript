a3520df2bb425ce7f0ad6009529d7902744f426c
Shafait98 <farhan98aiub@gmail.com>
2025-08-23 00:30:12 +0600

    explore array indexing

---
+++ array-index.js
@@ -0,0 +1,10 @@
+let cars = ["Porsha 911 gt","Lamborghini avendetor", "Ferrari 911 spider","Tesla","Toyata Supra Mk4","Nissan gtr r35"];
+
+console.log(cars[1]);
+
+let ferrari = cars[2];
+console.log(ferrari);
+
+cars[3] = "Land Cruiser Prado";
+
+console.log(cars[3]);
