406d9f99b77e829c2d4edce0671cca28d653f4b4
Shafait98 <farhan98aiub@gmail.com>
2025-08-23 01:08:15 +0600

    explored js pop OP

---
+++ array-pop.js
@@ -0,0 +1,8 @@
+let favSong = ["Pirroute","Low-life","Make you mine", "watch over you", "calm-down","every breath ypu take","summer time sadness"];
+favSong.pop();
+console.log(favSong);
+
+let out2 = favSong.pop();
+console.log(favSong);
+console.log(out2);
+
