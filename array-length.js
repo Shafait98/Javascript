531ee378ef208f3c119d012b5cb6075d741fc050
Shafait98 <farhan98aiub@gmail.com>
2025-08-23 00:23:14 +0600

    explored array travarsing with loop & array lenght

---
+++ array-length.js
@@ -0,0 +1,10 @@
+// let numbers = [22,23,41,55,60,34,54,29,33];
+
+// for(let num of numbers)
+// {
+//     console.log(num);
+// }
+
+let trip = [220,340,400,220,150,200,300,130];
+
+console.log(trip.length);
---
+++ loop-tasks.js
@@ -0,0 +1,40 @@
+/***
+
+"I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+
+ */
+
+/*programming hero*/
+
+
+// let i = 1;
+// let msg = "I will invest at least 6 hrs every single day for next 60 days!";
+
+// while(i<= 60 )
+// {
+//     console.log(msg);
+//     i++;
+// }
+
+
+/***
+
+Subtask-1:
+
+Find all the odd numbers from 61 to 100.
+
+ */
+
+let i = 61;
+
+console.log("The odd numbers are: ");
+
+while(i <= 100)
+{
+    i++;
+    if(i%2 === 1)
+    {
+        console.log(i);
+    }
+
+}
