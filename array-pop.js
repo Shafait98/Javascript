f777d1765b0f16d75832c55fcec9dc7e02c3b1df
Shafait98 <farhan98aiub@gmail.com>
2025-08-23 01:49:40 +0600

    explore for of in pop()  OP

--- array-pop.js
+++ array-pop.js
@@ -1,8 +1,15 @@
 let favSong = ["Pirroute","Low-life","Make you mine", "watch over you", "calm-down","every breath ypu take","summer time sadness"];
+
+console.log("favourite songs are: ")
+for(let song of favSong)
+{
+    console.log(,song);
+}
+
 favSong.pop();
 console.log(favSong);
 
-let out2 = favSong.pop();
+let out2 = favSong.pop(); // here we can store pop() element in a variable
 console.log(favSong);
 console.log(out2);
 
