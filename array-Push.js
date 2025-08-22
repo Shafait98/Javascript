9f067ecbdf473fe50ae484b3f6c8c07b67875cc3
Shafait98 <farhan98aiub@gmail.com>
2025-08-23 00:50:18 +0600

    add element in array by push OP

---
+++ array-Push.js
@@ -0,0 +1,9 @@
+let numbers = [2,3,5,6,8,12,14,16,19,22,33];
+numbers.push(35);
+numbers.push(36);
+numbers.push(37,55,63);
+console.log(numbers);
+
+let friends = ["rakib", "salek", "abir", "shafin", "prottush"];
+friends.push("fahim");
+console.log(friends);
