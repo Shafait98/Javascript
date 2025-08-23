4c55c9c784c330cfd4b3538053c9a08e152e267d
Shafait98 <farhan98aiub@gmail.com>
2025-08-24 03:22:32 +0600

    explore the indexOf() method & it's indexing

--- array-includes.js
+++ array-includes.js
@@ -1,10 +1,10 @@
 let friends = ["Shafin","Abir","Rakib", "Salek","Prottush"];
 
-console.log(friends.includes("Rakib")); //includes check if the value is stored and return true or false
+console.log(friends.includes("Rakib")); //includes() method check if the value is stored and return true or false
 
 
 
-if(friends.includes("salek")) // we can use includes() in condition also
+if(friends.includes("salek")) // we can use includes() method in condition also
 {
     console.log("get in!")
 }
