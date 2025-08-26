ad72ab5ec21fbabcae485ee5d4ebd9fdbb47e438
Shafait98 <farhan98aiub@gmail.com>
2025-08-27 02:17:46 +0600

    exploring the join() method

---
+++ array-isArray.js
@@ -0,0 +1,9 @@
+let smartGT = ["samsung","sony","panasonic","LG",];
+let Mug = [];
+
+let dessert = "brownie";
+
+console.log(Array.isArray(smartGT));
+console.log(Array.isArray(Mug));
+
+console.log(Array.isArray(dessert));
---
+++ array-methods.js
@@ -0,0 +1,10 @@
+let pLanguage = [`C++`, `java` ,`C#`];
+
+// console.log(pLanguage);
+
+// for(let ln of pLanguage)
+// {
+//     console.log(ln);
+// }
+
+console.log(pLanguage.join('  '));
