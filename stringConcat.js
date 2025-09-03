3c6b7741b81ce3eca8baad96290ade981bb80a6f
Shafait98 <farhan98aiub@gmail.com>
2025-09-04 02:27:06 +0600

    explored string concat & string includes

---
+++ stringConcat.js
@@ -0,0 +1,14 @@
+//*String Concat */
+
+let firstName = "Shafait";
+let lastName = "Ul Haque Siddique";
+
+console.log(firstName +" " +lastName);
+
+//*Concat using concat func */
+let fullName = firstName.concat(" " + lastName);
+console.log(fullName);
+
+//*check if the value is there in the string */
+console.log(firstName.includes('h'));
+console.log(lastName.includes('j'))
