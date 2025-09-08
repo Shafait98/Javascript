0c87e7d20b1d012a95ce0c5b49bde4b3bf27c5ff
Shafait98 <farhan98aiub@gmail.com>
2025-09-09 01:22:56 +0600

    updated

---
+++ object-properties.js
@@ -0,0 +1,14 @@
+const person = 
+{
+    name: 'Samanta',
+    age: 22,
+    contactNum: '+8801734843953',
+    mail: 'samantasiddique404@gmail.com',
+    bloodGroup: 'A+',
+    education: 'Bachelor of Business Administration',
+    status: 'Married'
+};
+
+let allContact = person.contactNum;
+
+console.log(allContact);
