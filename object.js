afed95e48c99e54cacf68e802c41496cf8fac185
Shafait98 <farhan98aiub@gmail.com>
2025-09-09 01:12:12 +0600

    explore object & it's properties

---
+++ object.js
@@ -0,0 +1,12 @@
+//** object declaration */
+
+const person ={
+    name: 'Farhan',
+    age: 25,
+    profession: 'Software Engineer',
+    status: 'Married'
+
+};
+
+console.log(person);
+console.log(person.age); // accessing data for specific property
