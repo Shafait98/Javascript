7b3b4d2fe7ed04c428d032253fbd44f1d38fad83
Shafait98 <farhan98aiub@gmail.com>
2025-10-02 02:09:33 +0600

    explore array in object, traversing the objects of an array & get set the values of object from an array

---
+++ array-object.js
@@ -0,0 +1,30 @@
+const employees = [
+    {name: ' Farhan', age: 25, designation: 'Software Engineer', salary: 30000},
+
+    {name:'shafait', age: 26, designation: 'Network Engineer', salary: 28000},
+
+    {name: 'Fahmid', age: 26, designation: 'Data Analyst', salary: 32000},
+
+    {name: 'Akib', age: 24, designation: 'Product Manager', salary: 30000}
+
+];
+
+console.log(employees[2]);
+console.log(employees[3].name, ' ', employees[3].age);
+console.log(employees[0].name, ' ', employees[0].age);
+console.log(employees[1].name, ' ', employees[1].age);
+
+
+employees[2].designation = 'Site Engineer';
+
+console.log(employees[2]);
+
+for(let emp of employees)
+{
+    console.log(emp);
+}
+
+for(let emp of employees)
+{
+    console.log('Age: ' + emp.age);
+}
