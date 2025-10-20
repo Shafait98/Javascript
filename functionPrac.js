f46c8caaa542cfb7d5bcc8fc8ba9199fb8efc0d2
Shafait98 <farhan98aiub@gmail.com>
2025-10-21 01:35:06 +0600

    doing some function practice

---
+++ functionPrac.js
@@ -0,0 +1,32 @@
+// add numbers function
+
+
+function addAll(a,b,c,d,e)
+{
+console.log(a,b,c,d,e);
+const sum = a+b+c+d+e;
+console.log('sum= ', sum);
+}
+
+//square function
+
+function square (num)
+{
+    const square = num * num;
+    console.log('the square of given number is: ', square);
+}
+
+// difference function
+function difference (num1,num2)
+{
+    const diff = num1 - num2;
+    console.log('the difference is: ',diff);
+}
+
+const fatherAge = 65;
+const myAge = 26;
+
+
+addAll(2,3,4,5,88);
+square(12);
+difference(fatherAge,myAge);
