dd5799099426ba0753f100e71054afff542fc880
Shafait98 <farhan98aiub@gmail.com>
2025-10-30 01:20:53 +0600

    some notes are added

--- array-object.js
+++ array-object.js
@@ -10,9 +10,9 @@ const employees = [
 ];
 
 console.log(employees[2]);
-console.log(employees[3].name, ' ', employees[3].age);
-console.log(employees[0].name, ' ', employees[0].age);
-console.log(employees[1].name, ' ', employees[1].age);
+console.log(employees[3].name, ' : ', employees[3].age);
+console.log(employees[0].name, ' : ', employees[0].age);
+console.log(employees[1].name, ' : ', employees[1].age);
 
 
 employees[2].designation = 'Site Engineer'; // changing the value of an object's property
@@ -24,7 +24,7 @@ for(let emp of employees)
     console.log(emp);
 }
 
-for(let emp of employees)
+for(let emp of employees)   // we can get specific data of an object
 {
     console.log('Age: ' + emp.age);
 }
