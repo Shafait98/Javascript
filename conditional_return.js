4e444e20f7a44b7adb4408e4c9fcffa498a75790
Shafait98 <farhan98aiub@gmail.com>
2025-11-04 04:23:26 +0600

    checking a function's two parameter's different is even or odd with booleans

---
+++ conditional_return.js
@@ -0,0 +1,39 @@
+// checking if the given number is even or odd
+
+
+function isEven(number) 
+{
+    if(number % 2 === 0)
+    {
+       return true;
+    }
+    else
+        {
+            return false;
+        }
+}
+
+let res = isEven(10);
+let res2 = isEven(7);
+console.log(res);
+console.log(res2);
+
+// checking if the given number's difference is odd or even with boolean
+
+function diffOdd(num1,num2)
+{
+    let diff = num1 - num2;
+    if(diff % 2 === 1)
+    {
+        console.log("the given number's difference is a odd number!");
+        return true;
+    }
+    else
+        {
+            console.log("the given number's difference is a even number")
+            return false;
+        }
+}
+
+let result = diffOdd(50,20);
+console.log(result);
